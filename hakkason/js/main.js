f52e9c65223e5e4b7c5745584d0e6f21d8f09875
Tomoharu yuyama <tomoharuyuyama@gmail.com>
2020-09-13 14:45:04 +0900

    ハッカソンの制作物です

---
+++ hakkason/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+    accessibility: true,
+    arrows: false,
+    autoplay: true, //自動再生
+    dots: false, //ドットのナビゲーションを表示
+    infinite: true, //スライドのループ有効化
+    speed: 1000, //切り替えのスピード（小さくすると速くなる）
+    fade: true, //フェードの有効化
+  });
+  });
